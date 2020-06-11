bbd9e3bfda3f6d70501264abbbd3d30469a36c6c
mrlpedroso <mrlpedroso@gmail.com>
2020-06-11 10:37:23 -0300

    Banner

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -6,6 +6,7 @@ import {Wrapper, Header, BalanceContainer, BalanceTittle, Balance, Container} fr
 import Suggestions from '../../components/Suggestions'
 import Activities from '../../components/Activities'
 import Tips from '../../components/Tips'
+import Banner from '../../components/Banner'
 
 export default function Home() {
     return (
@@ -24,6 +25,7 @@ export default function Home() {
                 <Suggestions />
                 <Activities />
                 <Tips />
+                <Banner />
             </Container>
         </Wrapper>
     )
