14b13e4914f8ebcf8937e71f15e176b201558f7e
mrlpedroso <mrlpedroso@gmail.com>
2020-06-11 08:22:28 -0300

    Inicio

---
+++ src/screens/Home/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { MaterialCommunityIcons, AntDesign } from '@expo/vector-icons'
+
+import {Wrapper, Header, BalanceContainer, BalanceTittle, Balance, Container} from './styles'
+
+import Suggestions from '../../components/Suggestions'
+
+export default function Home() {
+    return (
+        <Wrapper>
+            <Container>
+                <Header>
+                    <MaterialCommunityIcons name="qrcode-scan" size={30} color="#10c86e"/>
+
+                    <BalanceContainer>
+                        <BalanceTittle>Meu saldo</BalanceTittle>
+                        <Balance>R$ 0,00</Balance>
+                    </BalanceContainer>
+                    
+                    <AntDesign name="gift" size={30} color="#10c86e"/>
+                </Header>
+                <Suggestions/>
+            </Container>
+        </Wrapper>
+    )
+}
